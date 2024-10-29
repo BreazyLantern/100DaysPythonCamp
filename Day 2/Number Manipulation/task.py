a05ee59d3e2e09bb830998727427e03207df33cf
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-10-29 04:37:24 -0400

    Completed Day 2

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,20 @@
 bmi = 84 / 1.65 ** 2
+
+# flooring numbers
+print(int(bmi))
+
+# rounding
+print(round(bmi)) #one input example
+print(round(bmi, 2)) #two input example rounds to 2 decimal points of accuracy
+
+# number manipulation
+score = 0
+score += 1
+print(score)
+
+# f-strings
+height = 1.8
+is_winning = True
+print("Your score is " + f"Your score is = {score}, "
+                         f"your height is {height}. "
+                         f"Your are winning is {is_winning}")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,15 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+print(f"Answer should be {((150.00 / 5) * 1.12)}")
+
+print(f"{round((bill / people) * (tip/100 + 1), 2)}")
+
+# step by step method
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
 
