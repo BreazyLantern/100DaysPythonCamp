78fb1709e63131582bc03e0a335490ede247afc6
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-10-30 19:02:33 -0400

    completed day 8

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -1,3 +1,5 @@
+from encodings.utf_7 import encode, decode
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
@@ -11,6 +13,14 @@ shift = int(input("Type the shift number:\n"))
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
+def decrypt(original_text, shift_amount):
+    decrypt_text = ""
+    for letter in original_text:
+        shifted_pos = alphabet.index(letter) - 2
+        shifted_pos %= len(alphabet)
+        decrypt_text += alphabet[shifted_pos]
+    print(f"Here is the decoded result: {decrypt_text}")
+
 def encrypt(original_text, shift_amount):
     cipher_text = ""
     for letter in original_text:
@@ -20,7 +30,20 @@ def encrypt(original_text, shift_amount):
     print(f"Here is the encoded result: {cipher_text}")
 
 
-encrypt(original_text=text, shift_amount=shift)
 
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    for letter in original_text:
+
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode= direction)
 
 
