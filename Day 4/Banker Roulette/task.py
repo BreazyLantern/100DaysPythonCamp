ecd30545473b9e92d5683e977150979ac24b8e45
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-10-30 04:27:24 -0400

    Completed day 4

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,10 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# print out a random name from the list using randoms choice function
+print(random.choice(friends))
+
+# option 2
+print(friends[random.randint(0, 4)])
+
--- Day 4/IndexError/task.py
+++ Day 4/IndexError/task.py
@@ -7,3 +7,18 @@ states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Conne
                      "New Mexico", "Arizona", "Alaska", "Hawaii"]
 
 print(states_of_america)
+
+
+print(len(states_of_america))
+
+# creating errors on purpose
+# print(states_of_america[50])
+
+# dirty dozen example
+
+fruits = ["Cherry", "Apple", "Pear"]
+veg = ["Cucumber", "Kale", "Spinnach"]
+
+# this is an example of a nested list having a list that contains multiple lists
+fruits_and_veg = [fruits, veg]
+print(fruits_and_veg)
