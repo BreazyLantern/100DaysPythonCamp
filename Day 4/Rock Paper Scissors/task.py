dcc7ac1fcb8c16127269eba5661aaa5db7d3447d
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-10-30 19:33:57 -0400

    Start to day 9 Dictionaries and lists

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,5 +1,3 @@
-import random
-
 rock = '''
     _______
 ---'   ____)
@@ -26,31 +24,3 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
-
-# make a game of rock paper scissors
-computer_choice = random.randint(0,2)
-game_images = [rock, paper, scissors]
-
-player_choice = int(input("What hand will you play? [rock = 0, paper = 1, scissors = 2]\n"))
-if player_choice == 0:
-    print(rock)
-elif player_choice == 1:
-    print(paper)
-elif player_choice == 2:
-    print(scissors)
-
-print(f"The computer played:\n{game_images[computer_choice]}")
-
-if player_choice >= 3 or player_choice < 0:
-    print("Invalid input you lose")
-elif player_choice == 0 and computer_choice == 2:
-    print("You win")
-elif computer_choice == 0 and player_choice == 2:
-    print("You lose")
-elif computer_choice > player_choice:
-    print("You lose")
-elif player_choice > computer_choice:
-    print("You win")
-elif computer_choice == player_choice:
-    print("It's a tie")
