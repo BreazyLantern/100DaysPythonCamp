feb5578ee7c898b3a637314a017b0efd36c73243
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-10-30 22:22:33 -0400

    Completed day 10

--- Day 10/Dcostrings/task.py
+++ Day 10/Dcostrings/task.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    """This is an example of a doc string This function takes the first and last name and
+    formats the name with aa title case version of the name then returns the modified value"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
