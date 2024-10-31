525b072c90ed5c624061acc0bb9c7e1ca02b72db
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-10-30 20:19:08 -0400

    Small fix to day 9

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,6 +1,3 @@
-from symbol import return_stmt
-from zipfile import stringCentralDir
-
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
                           "Function": "A piece of code that you can easily call over and over again."}
 
