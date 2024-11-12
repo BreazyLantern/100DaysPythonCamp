90ce1d3b069dc95a372a3818a1029448be800c3e
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2024-11-12 05:12:49 -0500

    Day 18 more about turtle and importing tools for python. Created a painting from a jpg colorgrade values

---
+++ Day 18/Hurst_painting/Painting_Lesson.py
@@ -0,0 +1,67 @@
+# import colorgram
+
+# colors = colorgram.extract('Hirst.jpg', 425)
+
+"""Color Extraction code from image file"""
+from threading import Timer
+
+# rgb_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+
+
+"""Create a Hirst art piece from jpg colorgrade values using Turtle"""
+import turtle as t
+import random
+import time
+
+t.colormode(255)
+tim = t.Turtle()
+color_list = [(238, 236, 230), (193, 160, 121), (72, 92, 125), (141, 87, 59), (141, 160, 187), (216, 209, 121), (29, 33, 47), (182, 146, 162), (55, 32, 23), (175, 160, 42), (120, 75, 93), (139, 172, 152), (62, 29, 38), (78, 113, 79), (136, 27, 19), (117, 30, 43), (184, 100, 86), (48, 58, 93), (172, 100, 115), (31, 48, 43), (102, 120, 168), (101, 155, 93), (214, 174, 190), (216, 180, 173), (65, 81, 28), (164, 208, 187), (177, 186, 214), (219, 207, 10), (48, 73, 62), (40, 74, 81), (179, 197, 201), (112, 132, 141)]
+tim.speed("fastest")
+tim.hideturtle()
+
+t1 = time.time()
+tim.penup()
+
+"""My method of creating a Hirst painting"""
+# y = -220
+# tim.setpos(-220, y)
+# for __ in range(10):
+#     for _ in range(10):
+#         tim.dot(20, random.choice(color_list))
+#         tim.forward(50)
+#     y += 50
+#     tim.setpos(-220, y)
+# t2 = time.time()
+# print(t2 - t1)
+
+
+
+"""Instructors method"""
+"""Might be better for optimized big O performance"""
+tim. setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+t2 = time.time()
+print(t2 - t1)
+
+screen = t.Screen()
+screen.exitonclick()
---
+++ Day 18/main.py
@@ -0,0 +1,87 @@
+from random import choice, randint
+import turtle as t
+"""draw a square"""
+timmy_the_turtle = t.Turtle()
+
+# def draw_line(length, degree):
+#     timmy_the_turtle.forward(length)
+#     timmy_the_turtle.right(degree)
+#
+
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+# for i in range(4):
+#     draw_line(100, 90)
+
+# """Dash lines"""
+# for _ in range(4):
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.pendown()
+
+"""Draw polygons in succession of increasing side count"""
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(angle)
+
+colors = ["pale green", "yellow", "cornflower blue", "lime", "red", "magenta", "coral",
+          "dark orchid", "navy", "blue", "orange red"]
+
+# for shape_side_n in range(3, 11):
+#     timmy_the_turtle.color(choice(colors))
+#     draw_shape(shape_side_n)
+
+"""Draw random travel directions for the turtle"""
+directions = [0, 90, 180, 270]
+t.colormode(255)
+
+def random_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    c_tuple = (r, g, b)
+    return c_tuple
+
+timmy_the_turtle.speed("fastest")
+
+# timmy_the_turtle.pensize(10)
+# for _ in range(200):
+#     timmy_the_turtle.color(random_color())
+#     timmy_the_turtle.forward(30)
+#     timmy_the_turtle.setheading(choice(directions))
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/ size_of_gap)):
+        timmy_the_turtle.color(random_color())
+        timmy_the_turtle.circle(100)
+        timmy_the_turtle.setheading(timmy_the_turtle.heading() + size_of_gap)
+
+draw_spirograph(5)
+
+
+
+"""about tuples 
+tuples are like lists however you cannot modify the tuple once it is first created
+this is an immutable set"""
+
+a_tuple = (1, 3, 5)
+"""should you need a list from a tuple just use this"""
+list(a_tuple)
+
+"""Different way of importing
+Try avoiding this since it makes it difficult to identify from where a method or function
+come from"""
+# from turtle import *
+#
+# from random import *
+
+# import turtle as t
+# tim = t.Turtle()
+
+
+
+screen = t.Screen()
+screen.exitonclick()
