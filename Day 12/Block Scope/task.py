c95fef271a2f6903487afd9e577ea8cb6447fdc2
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-06-11 23:17:20 -0400

    edit day 12

--- Day 12/Block Scope/task.py
+++ Day 12/Block Scope/task.py
@@ -4,8 +4,9 @@
 game_level = 3
 enemies = ["skeleton", "zombie", "alien"]
 def create_enemy():
-    new_enemy = ""
     if game_level < 5:
         new_enemy = enemies[0]
 
     print(new_enemy)
+
+create_enemy()
