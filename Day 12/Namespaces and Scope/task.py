5c82d85f215b1907e389db63d9160186ef3258e2
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-06-11 23:12:58 -0400

    edit day 12

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -22,7 +22,7 @@ player_health = 10 # Global variable
 
 def drink_potion():
     potion_strength = 2 # Local variable
-    print(potion_strength)
+    print(player_health)
 
 drink_potion()
 print(player_health)
