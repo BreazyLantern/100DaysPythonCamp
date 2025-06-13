6d4b07d7732c7e942300d864ef6cba7f45f7f45e
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-06-12 20:18:43 -0400

    edit day 18 for hirst to change the column numbers.

--- Day 18/Hurst_painting/Painting_Lesson.py
+++ Day 18/Hurst_painting/Painting_Lesson.py
@@ -46,19 +46,20 @@ tim.penup()
 
 """Instructors method"""
 """Might be better for optimized big O performance"""
-tim. setheading(225)
+tim.setheading(225)
 tim.forward(300)
 tim.setheading(0)
 number_of_dots = 100
+i = .8
 for dot_count in range(1, number_of_dots + 1):
     tim.dot(20, random.choice(color_list))
-    tim.forward(50)
+    tim.forward(50*i)
 
-    if dot_count % 10 == 0:
+    if dot_count % (10*i) == 0:
         tim.setheading(90)
         tim.forward(50)
         tim.setheading(180)
-        tim.forward(500)
+        tim.forward(500*i**2)
         tim.setheading(0)
 t2 = time.time()
 print(t2 - t1)
