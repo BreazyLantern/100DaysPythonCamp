98c352d39f6fd5eb0138b36cb7549358f928ffd8
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-11-24 18:39:10 -0500

    Progression on day 24- file manipulation lesson - self attempt at snake reading and writing high score

--- Day 24 Local File Systems/Improved Snake/scoreboard.py
+++ Day 24 Local File Systems/Improved Snake/scoreboard.py
@@ -18,11 +18,17 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
