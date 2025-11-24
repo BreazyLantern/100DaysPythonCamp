48b8549095c2dab18842b9f61f6cfc4b6c9efee1
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-11-24 18:24:20 -0500

    Progression on day 24- file manipulation lesson

---
+++ Day 24 Local File Systems/Improved Snake/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+# constants
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+#additions of a high_score tracking ability to the game
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
---
+++ Day 24 Local File Systems/Saving Files with read write/main.py
@@ -0,0 +1,29 @@
+
+#store away and open the example textfile
+file = open("my_file.txt")
+
+#store the read contents of a file
+contents = file.read()
+
+print(contents)
+
+#close the file to reduce the number of resources used up while program is running, IE performance saving methods
+file.close()
+
+#secondary method
+#with method of opening a file will self manage the opened file to close after usage
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+#writing to the file
+#file operational modes: "a" appends to file, "r" read-only, "w" write to file
+with open("my_file.txt", mode="w") as file:
+    file.write("New Text. Written to the file.")
+
+with open("my_file.txt", mode= "a") as file:
+    file.write("\nappended text to file")
+
+#create a new file
+with open("new_file.txt", mode="w") as file:
+    file.write("new file")
