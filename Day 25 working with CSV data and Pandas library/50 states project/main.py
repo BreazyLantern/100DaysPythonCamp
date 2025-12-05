fc1af376060667afa1082233ce13a30edf4a0098
Tkim2repo <74295988+BreazyLantern@users.noreply.github.com>
2025-12-05 02:00:03 -0500

    Progression on day 25 - Pandas - 50 states project challenge attempt part 2

--- Day 25 working with CSV data and Pandas library/50 states project/main.py
+++ Day 25 working with CSV data and Pandas library/50 states project/main.py
@@ -54,6 +54,13 @@ else:
     while len(guessed_states) < 50:
         answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                         prompt="What's another state's name").title()
+        if answer_state == "Exit":
+            missing_states = []
+            for state in all_states:
+                missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv("states_to_learn.csv")
+            break
         if answer_state in all_states:
             guessed_states.append(answer_state)
             t = turtle.Turtle()
@@ -62,5 +69,7 @@ else:
             state_data = data[data.state == answer_state]
             t.goto(state_data.x.item(), state_data.y.item())
             t.write(answer_state)
+    # states to learn.csv
+
 
 screen.exitonclick()
